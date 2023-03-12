bff42e5750074770b200b4d8798b2364101cc9a7
Nicolas Hoizey <nicolas@hoizey.com>
2023-03-12 22:54:42 +0100

    feat: add transforms for responsive images and HTML minification

--- 11ty/transforms/html_min.js
+++ 11ty/transforms/html_min.js
@@ -1,15 +1,14 @@
 const htmlmin = require("html-minifier");
 
-module.exports = function htmlMinTransform(value, outputPath) {
-  if (!outputPath.endsWith(".html")) {
-    return value;
+module.exports = function htmlMinTransform(content) {
+  if (!this.page.outputPath || !this.page.outputPath.endsWith(".html")) {
+    return content;
   }
 
-  let minified = htmlmin.minify(value, {
+  return htmlmin.minify(content, {
     useShortDoctype: true,
     removeComments: true,
     collapseWhitespace: true,
     minifyCSS: false,
   });
-  return minified;
 };
--- eleventy-plugin-pack11ty.js
+++ eleventy-plugin-pack11ty.js
@@ -3,70 +3,105 @@
 const path = require("path");
 const glob = require("fast-glob");
 
-module.exports = (eleventyConfig) => {
-  /* ************************************************************
-  /* Add filters
-  /* ************************************************************ */
+module.exports = {
+  initArguments: {},
+  configFunction: (eleventyConfig, options = {}) => {
+    // Initialize options
+    options = Object.assign(
+      {
+        responsiver: false,
+        minifyHtml: false,
+      },
+      options
+    );
 
-  glob.sync(path.join(__dirname, "11ty/filters/*.js")).forEach((file) => {
-    let filters = require(file);
-    Object.keys(filters).forEach((name) => {
-      eleventyConfig.addFilter(name, filters[name]);
-    });
-  });
+    /* ************************************************************
+    /* Add filters
+    /* ************************************************************ */
 
-  /* ************************************************************
-  /* Add Nunjucks shortcodes
-  /* ************************************************************ */
-
-  glob.sync(path.join(__dirname, "11ty/shortcodes/*.js")).forEach((file) => {
-    let shortcodes = require(file);
-    Object.keys(shortcodes).forEach((name) => {
-      eleventyConfig.addNunjucksShortcode(name, shortcodes[name]);
-    });
-  });
-
-  /* ************************************************************
-  /* Add paired shortcodes
-  /* ************************************************************ */
-
-  glob
-    .sync(path.join(__dirname, "11ty/paired_shortcodes/*.js"))
-    .forEach((file) => {
-      let pairedShortcodes = require(file);
-      Object.keys(pairedShortcodes).forEach((name) => {
-        eleventyConfig.addPairedShortcode(name, pairedShortcodes[name]);
+    glob.sync(path.join(__dirname, "11ty/filters/*.js")).forEach((file) => {
+      let filters = require(file);
+      Object.keys(filters).forEach((name) => {
+        eleventyConfig.addFilter(name, filters[name]);
       });
     });
 
-  /* ************************************************************
-  /* Add plugins
-  /* ************************************************************ */
+    /* ************************************************************
+    /* Add Nunjucks shortcodes
+    /* ************************************************************ */
 
-  eleventyConfig.addPlugin(require("@11ty/eleventy-plugin-rss"));
-  eleventyConfig.addPlugin(require("@11ty/eleventy-plugin-syntaxhighlight"));
-  eleventyConfig.addPlugin(require("eleventy-plugin-embed-everything"), {
-    youtube: {
-      options: {
-        lite: {
-          css: {
-            // TODO: make it configurable?
-            path: "/assets/yt-lite/lite-yt-embed.css",
-          },
-          js: {
-            // TODO: make it configurable?
-            path: "/assets/yt-lite/lite-yt-embed.js",
+    glob.sync(path.join(__dirname, "11ty/shortcodes/*.js")).forEach((file) => {
+      let shortcodes = require(file);
+      Object.keys(shortcodes).forEach((name) => {
+        eleventyConfig.addNunjucksShortcode(name, shortcodes[name]);
+      });
+    });
+
+    /* ************************************************************
+    /* Add paired shortcodes
+    /* ************************************************************ */
+
+    glob
+      .sync(path.join(__dirname, "11ty/paired_shortcodes/*.js"))
+      .forEach((file) => {
+        let pairedShortcodes = require(file);
+        Object.keys(pairedShortcodes).forEach((name) => {
+          eleventyConfig.addPairedShortcode(name, pairedShortcodes[name]);
+        });
+      });
+
+    /* ************************************************************
+    /* Add plugins
+    /* ************************************************************ */
+
+    eleventyConfig.addPlugin(require("@11ty/eleventy-plugin-rss"));
+    eleventyConfig.addPlugin(require("@11ty/eleventy-plugin-syntaxhighlight"));
+    eleventyConfig.addPlugin(require("eleventy-plugin-embed-everything"), {
+      youtube: {
+        options: {
+          lite: {
+            css: {
+              // TODO: make it configurable?
+              path: "/assets/yt-lite/lite-yt-embed.css",
+            },
+            js: {
+              // TODO: make it configurable?
+              path: "/assets/yt-lite/lite-yt-embed.js",
+            },
           },
         },
       },
-    },
-  });
+    });
 
-  /* ************************************************************
-  /* Copy some assets
-  /* ************************************************************ */
+    if (options.responsiver !== false) {
+      // Make HTML images responsive
+      eleventyConfig.addPlugin(
+        require("eleventy-plugin-images-responsiver"),
+        options.responsiver
+      );
+    }
 
-  const passthroughCopyPaths = {};
-  passthroughCopyPaths[path.join(__dirname, "assets")] = "assets";
-  eleventyConfig.addPassthroughCopy(passthroughCopyPaths);
+    /* ************************************************************
+    /* Copy some assets
+    /* ************************************************************ */
+
+    const passthroughCopyPaths = {};
+
+    // Add Lite YouTube CSS and JS
+    passthroughCopyPaths[path.join(__dirname, "assets")] = "assets";
+
+    eleventyConfig.addPassthroughCopy(passthroughCopyPaths);
+
+    /* ************************************************************
+    /* Add transforms
+    /* ************************************************************ */
+
+    if (options.minifyHtml) {
+      // Minify HTML
+      eleventyConfig.addTransform(
+        "htmlmin",
+        require(path.join(__dirname, "11ty/transforms/html_min.js"))
+      );
+    }
+  },
 };
