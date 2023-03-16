363c278aa4a15b853eed0c8f4f408c3850d9b342
Nicolas Hoizey <nicolas@hoizey.com>
2023-03-17 00:38:43 +0100

    refactor: move Markdown-it configuration to a dedicated module

---
+++ 11ty/markdown.js
@@ -0,0 +1,105 @@
+// ------------------------------------------------------------------------
+// Markdown-it plugins and configurations
+// ------------------------------------------------------------------------
+
+const path = require('path');
+
+const slugify = require(path.join(__dirname, 'utils/slugify.js'));
+
+const markdownIt = require('markdown-it');
+const markdownItFootnote = require('markdown-it-footnote');
+const markdownItAnchor = require('markdown-it-anchor');
+const markdownItAttributes = require('markdown-it-attrs');
+const markdownItSpan = require('markdown-it-bracketed-spans');
+const markdownItContainer = require('markdown-it-container');
+const markdownItAbbr = require('markdown-it-abbr');
+
+const markdownItOptions = {
+	html: true,
+	breaks: true,
+	linkify: true,
+};
+
+const buildMarkdownIt = (options = {}) => {
+	// - - - - - - - - - - - - - - - - - - - - - - -
+	// Add anchor links to headings
+	// - - - - - - - - - - - - - - - - - - - - - - -
+	const markdownItAnchorOptions = {
+		level: [...Array(6).keys()].slice(options.firstLevel),
+		slugify,
+		permalink: markdownItAnchor.permalink.linkAfterHeader({
+			class: 'deeplink',
+			symbol: '&#xa7;&#xFE0E;', // https://www.toptal.com/designers/htmlarrows/punctuation/
+			style: 'visually-hidden',
+			visuallyHiddenClass: 'visually-hidden',
+			assistiveText: (title) => `Permalink to heading ${title}`,
+			wrapper: ['<div class="heading-wrapper">', '</div>'],
+		}),
+	};
+
+	// - - - - - - - - - - - - - - - - - - - - - - -
+	// Shift headings from Markdown
+	// Level 1 in Markdown becomes level options.firstLevel in HTML
+	// taken from https://gist.github.com/rodneyrehm/4feec9af8a8635f7de7cb1754f146a39
+	// - - - - - - - - - - - - - - - - - - - - - - -
+	function getHeadingLevel(tagName) {
+		if (tagName[0].toLowerCase() === 'h') {
+			tagName = tagName.slice(1);
+		}
+
+		return parseInt(tagName, 10);
+	}
+	function markdownItHeadingLevel(md, options) {
+		var firstLevel = options.firstLevel;
+
+		if (typeof firstLevel === 'string') {
+			firstLevel = getHeadingLevel(firstLevel);
+		}
+
+		if (!firstLevel || isNaN(firstLevel)) {
+			return;
+		}
+
+		var levelOffset = firstLevel - 1;
+		if (levelOffset < 1 || levelOffset > 6) {
+			return;
+		}
+
+		md.core.ruler.push('adjust-heading-levels', function (state) {
+			var tokens = state.tokens;
+			for (var i = 0; i < tokens.length; i++) {
+				if (tokens[i].type !== 'heading_close') {
+					continue;
+				}
+
+				var headingOpen = tokens[i - 2];
+				var headingClose = tokens[i];
+
+				var currentLevel = getHeadingLevel(headingOpen.tag);
+				var tagName = 'h' + Math.min(currentLevel + levelOffset, 6);
+
+				headingOpen.tag = tagName;
+				headingClose.tag = tagName;
+			}
+		});
+	}
+
+	// - - - - - - - - - - - - - - - - - - - - - - -
+	// Set Markdown-it options
+	// - - - - - - - - - - - - - - - - - - - - - - -
+	const mdIt = markdownIt(markdownItOptions);
+	mdIt.disable('code');
+	mdIt.use(markdownItHeadingLevel, options);
+	mdIt.use(markdownItFootnote);
+	mdIt.use(markdownItAnchor, markdownItAnchorOptions);
+	mdIt.use(markdownItAttributes);
+	mdIt.use(markdownItSpan);
+	mdIt.use(markdownItAbbr);
+	options.containers.forEach((container) =>
+		mdIt.use(markdownItContainer, container)
+	);
+
+	return mdIt;
+};
+
+module.exports = buildMarkdownIt;
--- eleventy-plugin-pack11ty.js
+++ eleventy-plugin-pack11ty.js
@@ -5,8 +5,6 @@ const path = require('path');
 const merge = require('deepmerge');
 const glob = require('fast-glob');
 
-const slugify = require(path.join(__dirname, '11ty/utils/slugify.js'));
-
 module.exports = (eleventyConfig, userOptions = {}) => {
 	// Initialize options
 	const defaultOptions = {
@@ -115,98 +113,6 @@ module.exports = (eleventyConfig, userOptions = {}) => {
 	// Markdown-it plugins and configurations
 	// ------------------------------------------------------------------------
 
-	const markdownIt = require('markdown-it');
-	const markdownItFootnote = require('markdown-it-footnote');
-	const markdownItAnchor = require('markdown-it-anchor');
-	const markdownItAttributes = require('markdown-it-attrs');
-	const markdownItSpan = require('markdown-it-bracketed-spans');
-	const markdownItContainer = require('markdown-it-container');
-	const markdownItAbbr = require('markdown-it-abbr');
-
-	const markdownItOptions = {
-		html: true,
-		breaks: true,
-		linkify: true,
-	};
-
-	// - - - - - - - - - - - - - - - - - - - - - - -
-	// Add anchor links to headings
-	// - - - - - - - - - - - - - - - - - - - - - - -
-	const markdownItAnchorOptions = {
-		level: [...Array(6).keys()].slice(options.markdown.firstLevel),
-		slugify,
-		permalink: markdownItAnchor.permalink.linkAfterHeader({
-			class: 'deeplink',
-			symbol: '&#xa7;&#xFE0E;', // https://www.toptal.com/designers/htmlarrows/punctuation/
-			style: 'visually-hidden',
-			visuallyHiddenClass: 'visually-hidden',
-			assistiveText: (title) => `Permalink to heading ${title}`,
-			wrapper: ['<div class="heading-wrapper">', '</div>'],
-		}),
-	};
-
-	// - - - - - - - - - - - - - - - - - - - - - - -
-	// Shift headings from Markdown
-	// Level 1 in Markdown becomes level options.markdown.firstLevel in HTML
-	// taken from https://gist.github.com/rodneyrehm/4feec9af8a8635f7de7cb1754f146a39
-	// - - - - - - - - - - - - - - - - - - - - - - -
-	function getHeadingLevel(tagName) {
-		if (tagName[0].toLowerCase() === 'h') {
-			tagName = tagName.slice(1);
-		}
-
-		return parseInt(tagName, 10);
-	}
-	function markdownItHeadingLevel(md, options) {
-		var firstLevel = options.firstLevel;
-
-		if (typeof firstLevel === 'string') {
-			firstLevel = getHeadingLevel(firstLevel);
-		}
-
-		if (!firstLevel || isNaN(firstLevel)) {
-			return;
-		}
-
-		var levelOffset = firstLevel - 1;
-		if (levelOffset < 1 || levelOffset > 6) {
-			return;
-		}
-
-		md.core.ruler.push('adjust-heading-levels', function (state) {
-			var tokens = state.tokens;
-			for (var i = 0; i < tokens.length; i++) {
-				if (tokens[i].type !== 'heading_close') {
-					continue;
-				}
-
-				var headingOpen = tokens[i - 2];
-				var headingClose = tokens[i];
-
-				var currentLevel = getHeadingLevel(headingOpen.tag);
-				var tagName = 'h' + Math.min(currentLevel + levelOffset, 6);
-
-				headingOpen.tag = tagName;
-				headingClose.tag = tagName;
-			}
-		});
-	}
-
-	// - - - - - - - - - - - - - - - - - - - - - - -
-	// Set Markdown-it options
-	// - - - - - - - - - - - - - - - - - - - - - - -
-	const mdIt = markdownIt(markdownItOptions);
-	mdIt.disable('code');
-	mdIt.use(markdownItHeadingLevel, {
-		firstLevel: options.markdown.firstLevel,
-	});
-	mdIt.use(markdownItFootnote);
-	mdIt.use(markdownItAnchor, markdownItAnchorOptions);
-	mdIt.use(markdownItAttributes);
-	mdIt.use(markdownItSpan);
-	mdIt.use(markdownItAbbr);
-	options.markdown.containers.forEach((container) =>
-		mdIt.use(markdownItContainer, container)
-	);
-	eleventyConfig.setLibrary('md', mdIt);
+	const buildMarkdownIt = require(path.join(__dirname, '11ty/markdown.js'));
+	eleventyConfig.setLibrary('md', buildMarkdownIt(options.markdown));
 };
