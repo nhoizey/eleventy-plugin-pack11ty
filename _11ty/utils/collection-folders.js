7e2e9b7ef91a07f0fe63f96cc3d1857fc237e3f7
Nicolas Hoizey <nicolas@hoizey.com>
2024-10-11 08:29:04 +0200

    Deal with non existing "collections" folder

--- _11ty/utils/collection-folders.js
+++ _11ty/utils/collection-folders.js
@@ -2,13 +2,15 @@ import fs from 'node:fs';
 
 export const folders = () => {
 	const foldersList = [];
-	fs.readdirSync('src/collections', {
-		encoding: 'utf8',
-		withFileTypes: true,
-	}).forEach((item) => {
-		if (item.isDirectory()) {
-			foldersList.push(item.name);
-		}
-	});
+	if (fs.existsSync('src/collections')) {
+		fs.readdirSync('src/collections', {
+			encoding: 'utf8',
+			withFileTypes: true,
+		}).forEach((item) => {
+			if (item.isDirectory()) {
+				foldersList.push(item.name);
+			}
+		});
+	}
 	return foldersList;
 };
