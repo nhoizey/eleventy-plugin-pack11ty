9074cbf16061b3bca9aea2ec675add5d3d2c4644
Nicolas Hoizey <nicolas@hoizey.com>
2024-10-29 16:36:58 +0100

    Add some filters

--- _11ty/filters/date.js
+++ _11ty/filters/date.js
@@ -14,11 +14,20 @@ const dateObj = (eleventyDate) => {
 	}).setLocale(locale);
 };
 
+// 14 October 1983
 export const readableDate = (date) =>
 	dateObj(date).toLocaleString(DateTime.DATE_FULL);
+
+// 14 October 1983
+export const readableDateTime = (date) =>
+	dateObj(date).toLocaleString(DateTime.DATETIME_FULL);
+
 
 // 1983-10-14
 export const attributeDate = (date) => dateObj(date).toISODate();
 
 // 1983-10-14T20:04:00.000Z
 export const isoDate = (date) => dateObj(date).toUTC().toISO();
+
+export const readableDateFromIso = (date) =>
+	DateTime.fromISO(date).toLocaleString(DateTime.DATETIME_FULL);
--- _11ty/filters/fs.js
+++ _11ty/filters/fs.js
@@ -1,5 +1,10 @@
+import fs from 'node:fs';
 import path from 'node:path';
 
 export const dirname = (filePath) => {
 	return path.dirname(filePath);
+};
+
+export const exists = (filePath) => {
+	return fs.existsSync(filePath);
 };
